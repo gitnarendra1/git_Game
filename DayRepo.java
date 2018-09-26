09e7dcf48c24e36c7fccb8482a46205a34c8512a
gitnarendra1 <43538183+gitnarendra1@users.noreply.github.com>
2018-09-26 23:35:54 +0530

    Update DayRepo.java

--- DayRepo.java
+++ DayRepo.java
@@ -6,11 +6,8 @@ import com.cg.beans.Day;
 import com.cg.exception.DuplicateElementException;
 
 public interface DayRepo {
-
-
-	public Day save(Day day) throws DuplicateElementException;
 	
-	//public Day findByPlayerName(String playerName);
+	public Day save(Day day) throws DuplicateElementException;
 	
 	public List<Day> findByGamesName(String gameName);
 }

abcda278c29b464a7f32befcebc8c913eb791b08
gitnarendra1 <43538183+gitnarendra1@users.noreply.github.com>
2018-09-26 23:57:37 +0530

    Update DayRepoImpl.java

--- Game/src/com/cg/repoImpl/DayRepoImpl.java
+++ Game/src/com/cg/repoImpl/DayRepoImpl.java
@@ -1,14 +1,54 @@
 package com.cg.repoImpl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import com.cg.bean.Day;
-import com.cg.repo.DayRepo;
+import com.cg.bean.Game;
+import com.cg.exception.DuplicateElementException;
 
-public class DayRepoImpl implements DayRepo {
+public class DayRepoImpl implements DayRepo{
 
-	@Override
-	public Day save(Day d) {
-		// TODO Auto-generated method stub
-		return null;
+	List<Day> days = new ArrayList<Day>();
+	
+	public Day save(Day day) throws DuplicateElementException {
+		if(day == null)
+			throw new NullPointerException();
+		if(day.getName()==null)
+			throw new NullPointerException();
+		
+		if(days.contains(day))
+			throw new DuplicateElementException("day object already present");
+		
+		days.add(day);
+		
+		return day;
 	}
+
+	public List<Day> findByGamesName(String gameName) {
+		if(gameName == null)
+			throw new NullPointerException();
+		List<Day> d1 = new ArrayList<Day>();
+		
+		for(Day d:days) {
+			
+			Set<Game> games=d.getGames();
+			
+			for(Game g:games) {
+				
+				String sname=g.getName();						
+				
+				if(sname==gameName)
+				{
+					d1.add(d);
+				}
+			}
+		}
+		
+		return d1;
+	}
+	
+	
 
 }

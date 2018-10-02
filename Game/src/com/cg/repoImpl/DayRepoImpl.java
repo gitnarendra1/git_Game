aabf1c521dda957d68ceb340bb603c35e692c87a
gitnarendra1 <43538183+gitnarendra1@users.noreply.github.com>
2018-10-02 21:43:17 +0530

    Update DayRepoImpl.java

--- Game/src/com/cg/repoImpl/DayRepoImpl.java
+++ Game/src/com/cg/repoImpl/DayRepoImpl.java
@@ -2,37 +2,22 @@ package com.cg.repoImpl;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import com.cg.bean.Day;
-import com.cg.bean.Game;
-import com.cg.repo.DayRepo;
 
 public class DayRepoImpl implements DayRepo{
-	private Set<Day> day;
 
-	public DayRepoImpl() {
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
+	List<Day> day = new ArrayList<Day>();
+	
 	public Day save(Day day) {
-		// TODO Auto-generated method stub
-		return day;
-	}
-
-	public Day findByName(String name) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-	public Set<Day> getday() {
-		return day;
+	
+	public List<Day> findByGamesName(String gameName) {
+		// TODO Auto-generated method stub
+		return null;
 	}
-
-	public void setGames(Set<Day> day) {
-		this.day = day;
-	}		
 	
 	
 

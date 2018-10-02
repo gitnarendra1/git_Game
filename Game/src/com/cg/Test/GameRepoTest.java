7808f701bd3bd92e9f33ef089b0cd772eb0a0d48
gitnarendra1 <43538183+gitnarendra1@users.noreply.github.com>
2018-10-02 21:32:24 +0530

    Update GameRepoTest.java

--- Game/src/com/cg/Test/GameRepoTest.java
+++ Game/src/com/cg/Test/GameRepoTest.java
@@ -1,27 +1,85 @@
-package com.cg.Test;
+package com.cg.test;
 
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 
 import com.cg.bean.Game;
+import com.cg.bean.Player;
+import com.cg.exception.DuplicateElementException;
 import com.cg.repo.GameRepo;
-import com.cg.service.GameService;
+import com.cg.repoimpl.GameRepoImpl;
+import com.cg.repo.PlayerRepo;
+import com.cg.repoimpl.PlayerRepoImpl;
 
 public class GameRepoTest {
+
 	
-	private GameRepo repo;	
+	private GameRepo gameRepo;
 	
-	@Test
-	public Game test_save(Game g)
-	{
-		return g;
+	@Before
+	public void init(){
 		
+		gameRepo = new GameRepoImpl();
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void test_save() {
+		Game game = new Game();
+		gameRepo.save(game);
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void test_save1() {
+		Game game = null;		
+		gameRepo.save(game);
+	}
+	
+	@Test(expected=DuplicateElementException.class)
+	public void test_save2() {
+		Game game = new Game();
+		game.setName("hockey");
+		gameRepo.save(game);
+		
+		Game game1 = new Game();
+		game1.setName("hockey");
+		gameRepo.save(game1);
+	}
+	
+
+	@Test(expected=NullPointerException.class)
+	public void test_findByName() {
+		String gameName =null;
+		gameRepo.findByName(gameName);
 	}
 	
 	@Test
-	public Game test_findByName(String name)
-	{
-		return null;
+	public void test_findByGamesName1() {
+		Game game = new Game();
+		game.setName("hockey");
 		
+		gameRepo.save(game);
+		
+		String GameName ="hockey";
+		Game g =gameRepo.findByName(GameName);
+	
+		assertTrue("hockey".equals(g.getName()));
 	}
+	
+	@Test
+	public void test_findByGamesName2() {
+		Game game = new Game();
+		game.setName("hockey");
+		
+		gameRepo.save(game);
+		
+		String GameName ="cricket";
+		Game g =gameRepo.findByName(GameName);
+	
+		assertNull(g);
+	}
+
 }

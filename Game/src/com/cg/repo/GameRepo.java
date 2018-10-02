edf35c01e3cf704cb6f5270a68307d30a1bdce1c
gitnarendra1 <43538183+gitnarendra1@users.noreply.github.com>
2018-10-02 21:39:18 +0530

    Update GameRepo.java

--- Game/src/com/cg/repo/GameRepo.java
+++ Game/src/com/cg/repo/GameRepo.java
@@ -3,6 +3,8 @@ package com.cg.repo;
 import com.cg.bean.Game;
 
 public interface GameRepo {
-public Game save(Game g);
-public Game findByName(String name);
+
+	public Game save(Game game);
+	
+	public Game findByName(String name);
 }
